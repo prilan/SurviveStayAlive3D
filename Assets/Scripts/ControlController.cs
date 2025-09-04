feee57f0f5c888e1a116f827b9e8d4cb51344aba
Ilya Pronin <ilya.pronin.broccoligames@gmail.com>
2025-09-04 12:18:37 +0400

    Разделение логики паузы и переключения между игроками

--- Assets/Scripts/ControlController.cs
+++ Assets/Scripts/ControlController.cs
@@ -9,13 +9,6 @@ namespace SurviveStayAlive
             if (Input.GetKeyDown(KeyCode.Escape)) {
                 AppModel.Instance.LogicState.PauseUnPauseGame();
             }
-
-            if (AppModel.Instance.LogicState.CurrentLogicState == LogicStateEnum.PauseState)
-                return;
-
-            if (Input.GetKeyDown(KeyCode.Tab)) {
-                PlayersManager.Instance.SetNextCurrentPlayer();
-            }
         }
     }
 }
---
+++ Assets/Scripts/PlayerSwitchController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace SurviveStayAlive
+{
+    public class PlayerSwitchController : MonoBehaviour
+    {
+        private void Update()
+        {
+            if (AppModel.Instance.LogicState.CurrentLogicState == LogicStateEnum.PauseState)
+                return;
+
+            if (Input.GetKeyDown(KeyCode.Tab)) {
+                PlayersManager.Instance.SetNextCurrentPlayer();
+            }
+        }
+    }
+}
